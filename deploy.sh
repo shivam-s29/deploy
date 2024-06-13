c62a31241c6077cad202846ccab0ace623fc2ba1
shivam-s29 <153683472+shivam-s29@users.noreply.github.com>
2024-06-13 11:25:16 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
-# Install python3-pip
 sudo yum install python3-pip -y
-
-# Clone the repository
-git clone https://github.com/shivam-s29/flight-perdiction.git
-cd flight-perdiction
-
-# Install the required Python packages
-sudo pip3 install -r requirements.txt
-
-# Run the app.py script in a detached screen session
-screen -m -d python3 app.py
-
+https://github.com/shivam-s29/Agri.git
+cd Agri
+pip3 install -r requirements.txt
+screen -m -d python3 app.py 
