d40c081aa3a70f2a526af54c6da91e2ac97b3efa
shivam-s29 <153683472+shivam-s29@users.noreply.github.com>
2024-06-13 10:41:23 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+# Install python3-pip
 sudo apt-get install python3-pip -y
+
+# Clone the repository
 git clone https://github.com/shivam-s29/Agri.git
 cd Agri
-pip3 install -r requirements.txt
-screen -m -d python3 app.py 
+
+# Install the required Python packages
+sudo pip3 install -r requirements.txt
+
+# Run the app.py script in a detached screen session
+screen -dmS agrisession python3 app.py
+
